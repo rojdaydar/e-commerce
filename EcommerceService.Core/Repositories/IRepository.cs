433d82377a59d9683115c0aaf77b48c37c8e0648
Rojda Çiçek Aydar <aydarrojda@gmail.com>
2022-02-19 21:11:54 +0300

    i added repository interface in the core layer

---
+++ EcommerceService.Core/Repositories/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using EcommerceService.Core.Domain;
+
+namespace EcommerceService.Core.Repositories;
+
+public interface IRepository<TEntity> where TEntity : Base
+{
+    IQueryable<TEntity> Query();
+
+    Task<ICollection<TEntity>> GetAllAsync();
+
+    TEntity GetById(int id);
+
+    Task<TEntity> GetByIdAsync(int id);
+
+    TEntity Find(Expression<Func<TEntity, bool>> match);
+
+    Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> match);
+
+    ICollection<TEntity> FindAll(Expression<Func<TEntity, bool>> match);
+
+    Task<ICollection<TEntity>> FindAllAsync(Expression<Func<TEntity, bool>> match);
+
+    TEntity Add(TEntity entity);
+
+    TEntity Update(TEntity updated);
+
+    void Delete(TEntity t);
+
+    int SaveChanges();
+
+    Task<int> SaveChangesAsync();
+}
