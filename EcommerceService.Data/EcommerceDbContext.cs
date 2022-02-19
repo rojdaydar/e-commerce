7d3f77f77e44a8d3d7bf5538bcc5de7dc92f8a47
Rojda Çiçek Aydar <aydarrojda@gmail.com>
2022-02-19 21:16:46 +0300

    i added class which it is name EcommerceDbContext

---
+++ EcommerceService.Data/EcommerceDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EcommerceService.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+namespace EcommerceService.Data;
+
+public class EcommerceDbContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Campaign> Campaigns { get; set; }
+        
+    public EcommerceDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>();
+        modelBuilder.Entity<Product>();
+        modelBuilder.Entity<Campaign>();
+        base.OnModelCreating(modelBuilder);
+    }
+}
